ef2850c0f906a102a41b180ace9a8a4f7e074f17
Nohajaina RAMAMONJISOA <arivonyramamonjisoa5@gmail.com>
2025-05-22 17:11:33 +0300

    second commit

---
+++ app/Livewire/Panier.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Panier extends Component
+{
+    public function render()
+    {
+        return view('livewire.panier');
+    }
+}
